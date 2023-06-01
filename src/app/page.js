1d4ed7940f7ac0ef9e432ea2f9dc1a6e99b7ae3f
garciazapiain <juangarciazapiain@gmail.com>
2023-06-01 15:54:22 +0200

    loading screen first test

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+"use client"; // This is a client component 👈🏽
 import Image from 'next/image'
 import BannerInicio from './components/BannerInicio';
 import SobreNosotros from './components/SobreNosotros';
@@ -6,17 +7,35 @@ import NavBar from './components/NavBar';
 import Catering from './components/Catering';
 import Contacto from './components/Contacto';
 import Footer from './components/Footer';
+import { useEffect, useState } from 'react';
+import Loading from './components/Loading';
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading for 3 seconds
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <main>
-      <NavBar/>
-      <BannerInicio/>
-      <Menu/>
-      <Catering/>
-      <SobreNosotros/>
-      <Contacto/>
-      <Footer/>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <>
+          <NavBar />
+          <BannerInicio />
+          <Menu />
+          <Catering />
+          <SobreNosotros />
+          <Contacto />
+          <Footer />
+        </>
+      )}
     </main>
   )
 }
